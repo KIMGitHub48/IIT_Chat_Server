9ad43dc733d8e1f6d923b0b4d806717986eed2e9
KIMGitHub48 <85426445+KIMGitHub48@users.noreply.github.com>
2025-05-25 21:36:21 +0700

    _

---
+++ Globals/core.gd
@@ -0,0 +1,8 @@
+extends Node
+
+
+@onready var CORE = get_node("/root/Core")
+@onready var operation_node = get_node("/root/Core/Operation")
+@onready var server_ui_node = get_node("/root/Core/ServerUI")
+@onready var net_node = get_node("/root/Core/Net")
+@onready var database_node = get_node("/root/Core/Database")
